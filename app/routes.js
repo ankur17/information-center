990800277aee04129a6de693d1d4e502f471e8ee
ankur17 <anankur17@gmail.com>
2019-06-12 18:16:37 +0530

    server routes added

--- app/routes.js
+++ app/routes.js
@@ -5,15 +5,41 @@ module.exports = function(app, passport) {
     app.use('/dist', require('compression')(), express.static(__dirname + '/dist'));
     app.use('/src', express.static(__dirname + '/src', { maxAge: 10000 }));
 
+    // google auth routes
+    app.post('/auth/check', passport.authenticate('local',{
+        successRedirect: '/view',
+        failureRedirect: '/login'})
+    ),function(req, res) {
+        res.sent({ user : req.user });
+    };
+
+
+    app.get('/login', function(req, res) {
+        res.render('login.ejs', { obj: _obj , user:req.user});
+    });
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/login');
+    });
+
+
+    app.get('/',(req, res)=>{
+        // res.redirect('/view');
+        if (req.isAuthenticated()) {
+            res.redirect('/view');
+        } else {
+            res.redirect('/login');
+        }
+    });
+
+    app.use('/view', controllers.UIController);
     app.use('/ajax', controllers.AjaxController);
 
     app.get("/health", function(req, res) {
         res.send("Application is up and running!!");
     });
 
-    app.get("/robots.txt", function(req, res) {
-        res.send("User-agent: *\nDisallow: /");
-    });
 
     app.get("/*", function(req, res) {
         res.send("Path doesn't exist");
