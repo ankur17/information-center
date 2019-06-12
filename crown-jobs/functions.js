c921c23fdd3a643ae359fdc10156337c21069d1f
ankur17 <anankur17@gmail.com>
2019-06-13 00:30:30 +0530

    Crown job for company unique user page view count

---
+++ crown-jobs/functions.js
@@ -0,0 +1,39 @@
+/**
+ * Created by ankur on 13/6/19.
+ */
+
+
+var cron = require('node-cron');
+var admin = require("firebase-admin");
+var serviceAccount = require("./keys/firebase-admin-key.json");
+
+admin.initializeApp({
+    databaseURL: "https://information-center-be32b.firebaseio.com",
+    credential: admin.credential.cert(serviceAccount),
+});
+
+var db = admin.database();
+
+var companyViewsRef = db.ref("/public/company/views");
+var companyInfoRef = db.ref("/public/company/info");
+
+
+const company_names = ["pratilipi","pratilipi_1","pratilipi_2","pratilipi_3"]
+
+
+cron.schedule('*/2 * * * *', () => {
+    console.log('running a task every two minutes');
+
+    company_names.forEach((company)=>{
+        companyViewsRef.child(company).once('value',function(snap){
+            let data = snap.val()
+            let size = Object.keys(data).length
+            // update the value to company info
+            companyInfoRef.child(company).update({
+                page_views: size
+            })
+
+        })
+    })
+
+});
